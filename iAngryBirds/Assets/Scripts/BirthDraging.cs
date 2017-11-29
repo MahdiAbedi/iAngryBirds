cbe7269677cbd752947159fa684c7a35a3602202
Mahdi Abedi <mahdiabedi220@yahoo.com>
2017-11-29 16:52:01 +0330

    velocity
    error in velocity has still remind

--- iAngryBirds/Assets/Scripts/BirthDraging.cs
+++ iAngryBirds/Assets/Scripts/BirthDraging.cs
@@ -6,25 +6,56 @@ public class BirthDraging : MonoBehaviour {
 
     public LineRenderer leftLineRenderer;
     public LineRenderer rightLineRenderer;
-    public float maxStrechProjectile=3.0f;
+    public float maxStrechLimit=1.5f;
+    public Transform SlingShot;
 
     private SpringJoint2D spring;
     private bool isClicked= false;
+    private Ray mouseRay;
+    private Rigidbody2D projectile;
+    private Vector2 oldVelocity=new Vector2(10,10);
 
 
     void Awake()
     {
+       
         spring = GetComponent<SpringJoint2D>();
+        SlingShot = spring.connectedBody.transform;
+        mouseRay = new Ray(SlingShot.position, Vector3.zero);
+        projectile = GetComponent<Rigidbody2D>();
     }
+
 	// Use this for initialization
 	void Start () {
+        
         LineRendereSetup();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        LineRendererUpdate();
+        //Debug.Log(projectile.velocity);
+        if (isClicked)
+        {
+            LineRendererUpdate();
+        }
+
+        if (spring!=null)
+        {
+           
+            if (!projectile.isKinematic && oldVelocity.sqrMagnitude>projectile.velocity.sqrMagnitude)
+            {
+                Destroy(spring);
+                projectile.velocity = oldVelocity;
+            }
+
+            if (!isClicked)
+            {
+               // Debug.Log("step 2");
+                oldVelocity = projectile.velocity;
+            }
+        }
+
 
     }
 
@@ -44,20 +75,33 @@ public class BirthDraging : MonoBehaviour {
 
     void OnMouseDown()
     {
+       
         spring.enabled = false;
         isClicked = true;
     }
 
     void OnMouseUp()
     {
+      
         spring.enabled = true;
-        GetComponent<Rigidbody2D>().isKinematic = false;
+        projectile.isKinematic = false;
         isClicked = false;
     }
 
     void LineRendererUpdate()
     {
-        leftLineRenderer.SetPosition(1,Input.mousePosition);
-        rightLineRenderer.SetPosition(1,Input.mousePosition);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mouseToSligShot = mousePosition - SlingShot.position;
+        if (mouseToSligShot.sqrMagnitude>maxStrechLimit*maxStrechLimit)
+        {
+            mouseRay.direction = mouseToSligShot;
+            mousePosition = mouseRay.GetPoint(maxStrechLimit);
+
+        }
+        mousePosition.z = 0;
+        transform.position = mousePosition;
+
+       // leftLineRenderer.SetPosition(1,Input.mousePosition);
+       // rightLineRenderer.SetPosition(1,Input.mousePosition);
     }
 }
